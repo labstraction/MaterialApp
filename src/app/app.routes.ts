569ba5903b5fa44811572d7f25aa4d2ecdf7fd3b
labstraction <labstraction@protonmail.com>
2026-02-16 12:23:59 +0100

    dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home-component/home-component';
 import { ListComponent } from './components/list-component/list-component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path:"", redirectTo:"/home", pathMatch:"full"},
     {path:"home", component:HomeComponent},
     {path:"list", component:ListComponent},
+    {path:"dashboard", component:DashboardComponent},
     {path:"**", redirectTo:"/home", pathMatch:"full"}
 ];
--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -13,10 +13,33 @@ import { RouterLink } from "@angular/router";
 export class HeaderComponent {
 
 
+  paletteId = 0;
   isDarkMode = false;
 
   switchMode() {
-    throw new Error('Method not implemented.');
+    // if (this.isDarkMode) {
+    //   this.isDarkMode = false;
+    // } else {
+    //   this.isDarkMode = true;
+    // }
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-body');
   }
 
+  switchPalette() {
+    this.paletteId++;
+    if (this.paletteId > 2) {
+      this.paletteId = 0;
+    }
+    if (this.paletteId === 1) {
+      document.body.classList.add('charteuse-theme');
+    } else if (this.paletteId === 2) {
+      document.body.classList.remove('charteuse-theme');
+      document.body.classList.add('orange-theme');
+    } else {
+      document.body.classList.remove('charteuse-theme');
+      document.body.classList.remove('orange-theme');
+    }
+    
+  }
 }
